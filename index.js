e343605f205aaadc9d6270188200ab72e41f436a
Utkarsh Agnihotri <utkarshagnihotri7@gmail.com>
2024-01-15 16:39:38 +0530

    auth and post routes done

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { connection } = require("./db");
 const { userRouter } = require("./routes/user.router");
-
+const { postRouter } = require("./routes/post.routes");
+const { auth } = require("./middleware/auth.middleware");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 
 app.use("/users", userRouter);
-// app.use("/posts", postRouter);
+app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Home Page");
@@ -16,7 +18,7 @@ app.listen(8081, async () => {
   try {
     await connection;
     console.log("connected to db");
-    console.log("server is running ar port 8080");
+    console.log("server is running ar port 8081");
   } catch (err) {
     console.log(err);
   }
---
+++ routes/post.routes.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { PostModel } = require("../models/post.model");
+const { auth } = require("../middleware/auth.middleware");
+const mongoose = require("mongoose");
+const postRouter = express.Router();
+postRouter.use(auth);
+
+postRouter.post("/add", async (req, res) => {
+  try {
+    const post = new PostModel(req.body);
+    await post.save();
+    res.status(200).json({ msg: "a new Post has been created" });
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+});
+
+postRouter.get("/", async (req, res) => {
+  try {
+    const posts = await PostModel.find({
+      userId: req.body.userId,
+    });
+    res.status(200).json({ post: posts });
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
+});
+
+postRouter.patch("/update/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const payload = req.body;
+  try {
+    const post = await PostModel.findOne({ _id: PostId });
+    if (post.userId === req.body.userId) {
+      await PostModel.findByIdAndUpdate({ _id: postId }, payload);
+      res.status(200).json({ msg: "user has been updated" });
+    }
+  } catch (err) {
+    res.status(400).json({ err: err });
+  }
+});
+
+postRouter.delete("/delete/:postId", async (req, res) => {
+  const { postId } = req.params;
+
+  try {
+    const post = await PostModel.findOne({ _id: PostId });
+    if (post.userId === req.body.userId) {
+      await PostModel.findByIdAndDelete({ _id: postId });
+      res.status(200).json({ msg: "user has been deletede" });
+    }
+  } catch (err) {
+    res.status(400).json({ err: err });
+  }
+});
